a1bc18e71c48df1dd12146c42e49758199acae18
ShionKoga <starboost0616@icloud.com>
2024-05-27 18:08:41 +0900

    Jwtエンコーダーとデコーダーを実装した

---
+++ server/src/main/kotlin/com/security/server/auth/OriginalJwtEncoder.kt
@@ -0,0 +1,36 @@
+package com.security.server.auth
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class OriginalJwtEncoder(
+    @Value("\${app.jwt.secret}")
+    private val secret: String,
+    @Value("\${app.jwt.expiration-time}")
+    private val expirationTime: Int,
+    @Value("\${app.jwt.issuer}")
+    private val issuer: String,
+) {
+    private val key = Keys.hmacShaKeyFor(secret.toByteArray())
+    fun encode(userRecord: UserRecord): String {
+        val issuedAt = System.currentTimeMillis()
+        val expiresAt = issuedAt + expirationTime * 1000
+        val claims = Jwts.claims()
+            .id(UUID.randomUUID().toString())
+            .subject(userRecord.subject)
+            .expiration(Date(expiresAt))
+            .issuer(issuer)
+            .issuedAt(Date(issuedAt))
+            .add("name", userRecord.username)
+            .add("userId", userRecord.id)
+            .build()
+        return Jwts.builder()
+            .claims(claims)
+            .signWith(key)
+            .compact()
+    }
+}
---
+++ server/src/test/kotlin/com/security/server/auth/OriginalJwtDecoderTests.kt
@@ -0,0 +1,79 @@
+package com.security.server.auth
+
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.IncorrectClaimException
+import io.jsonwebtoken.security.SignatureException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class OriginalJwtDecoderTests {
+    val testSecret = "secret-string-for-jwt-decoding-test"
+
+    @Test
+    fun アクセストークンをparseしてUserRecordを返す() {
+        val userRecord = UserRecord(subject = "subject", username = "yusuke")
+        val encoder = OriginalJwtEncoder(testSecret, 3600, "https://hoge.com")
+        val accessToken = encoder.encode(userRecord)
+
+
+        val sut = OriginalJwtDecoder(testSecret, "https://hoge.com")
+        val result = sut.decode(accessToken)
+
+
+        assertEquals(userRecord, result)
+    }
+
+    @Test
+    fun 有効期限が切れている場合エラーを投げる() {
+        val userRecord = UserRecord(subject = "subject", username = "yusuke")
+        val encoder = OriginalJwtEncoder(testSecret, 1, "https://hoge.com")
+        val accessToken = encoder.encode(userRecord)
+
+
+        Thread.sleep(1200)
+        val sut = OriginalJwtDecoder(testSecret, "https://hoge.com")
+
+
+        assertThrows<ExpiredJwtException> { sut.decode(accessToken) }
+    }
+
+    @Test
+    fun Issuerが違う場合エラーを投げる() {
+        val userRecord = UserRecord(subject = "subject", username = "yusuke")
+        val encoder = OriginalJwtEncoder(testSecret, 3600, "https://hoge.com")
+        val accessToken = encoder.encode(userRecord)
+
+
+        val sut = OriginalJwtDecoder(testSecret, "https://example.com")
+
+
+        assertThrows<IncorrectClaimException> { sut.decode(accessToken) }
+    }
+
+    @Test
+    fun シークレットが違う場合エラーを投げる() {
+        val userRecord = UserRecord(subject = "subject", username = "yusuke")
+        val encoder = OriginalJwtEncoder(testSecret + "1", 3600, "https://hoge.com")
+        val accessToken = encoder.encode(userRecord)
+
+
+        val sut = OriginalJwtDecoder(testSecret, "https://hoge.com")
+
+
+        assertThrows<SignatureException> { sut.decode(accessToken) }
+    }
+
+    @Test
+    fun アクセストークンが改ざんされている場合エラーを投げる() {
+        val userRecord = UserRecord(subject = "subject", username = "yusuke")
+        val encoder = OriginalJwtEncoder(testSecret, 3600, "https://hoge.com")
+        val accessToken = encoder.encode(userRecord)
+
+
+        val sut = OriginalJwtDecoder(testSecret, "https://hoge.com")
+
+
+        assertThrows<SignatureException> { sut.decode(accessToken + "a") }
+    }
+}
