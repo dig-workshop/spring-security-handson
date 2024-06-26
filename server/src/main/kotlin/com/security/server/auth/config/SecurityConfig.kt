309c9060684d3f0e73c93a776f2494cc298fdfdc
Shion Koga <shion_koga@mail.toyota.co.jp>
2024-06-26 11:32:06 +0900

    第0ハンズオンのゴール

--- server/src/main/kotlin/com/security/server/auth/config/SecurityConfig.kt
+++ server/src/main/kotlin/com/security/server/auth/config/SecurityConfig.kt
@@ -1,9 +1,13 @@
 package com.security.server.auth.config
 
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -11,7 +15,27 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { it.disable() }
+        http.authorizeHttpRequests {
+            it.requestMatchers("/auth/api/users/me").authenticated()
+        }
+        http.oauth2Login {
+            it.defaultSuccessUrl("http://localhost:5173", true)
+        }
+        http.exceptionHandling {
+            it.authenticationEntryPoint(CustomAuthenticationEntryPoint())
+        }
+        http.logout { it.logoutSuccessUrl("http://localhost:5173") }
         return http.build()
     }
 }
 
+class CustomAuthenticationEntryPoint: AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException
+    ) {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+    }
+}
